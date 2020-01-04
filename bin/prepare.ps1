3878a68944ec0218a6ce66beee3a1cd106128ec9
Mikhail Tretiakov <8926238@gmail.com>
2020-01-04 18:22:48 +0000

    'initial'

---
+++ bin/prepare.ps1
@@ -0,0 +1,2 @@
+"C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\VC\Auxiliary\Build\vcvars64.bat"
+ruby "C:\Users\ruby-packer\ruby-packer\bin\rubyc" "C:\Users\ruby-packer\ruby-packer-test\app.rb" -c -o "C:\Users\ruby-packer\ruby-packer-build\ruby-packer-test-app.exe" --nmake-args=CC=clang-cl
